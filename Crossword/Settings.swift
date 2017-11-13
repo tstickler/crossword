15cfb11568327d3bcac66c7f9019238411a1f3ba
tyler <tstickler17@yahoo.com>
2017-11-13 13:40:06 -0800

    Added settings

---
+++ Crossword/Settings.swift
@@ -0,0 +1,18 @@
+//
+//  Settings.swift
+//  Crossword
+//
+//  Created by Tyler Stickler on 11/13/17.
+//  Copyright © 2017 tstick. All rights reserved.
+//
+
+import UIKit
+
+class Settings {
+    static var musicEnabled: Bool = true
+    static var soundEffects: Bool = true
+    static var showTimer: Bool = true
+    static var skipFilledSquares: Bool = true
+    static var lockCorrect: Bool = true
+    static var correctAnim: Bool = true
+}
